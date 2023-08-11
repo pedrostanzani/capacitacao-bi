2f8ec4ac0106150fcf9a751086c5f159ce865294
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-08-11 09:07:21 -0300

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-green-700 text-white text-center min-h-screen">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
